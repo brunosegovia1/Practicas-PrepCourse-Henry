5837385a8de72452c2ec4b56bf1d406f19de094a
Bruno Segovia <brunosegovia2009@gmail.com>
2024-01-18 15:23:06 -0300

    Fundamentals of Javascript FINISHEDgit add .git add .!

--- M05 Fundamentos Javascript/ejercicios/10.js
+++ M05 Fundamentos Javascript/ejercicios/10.js
@@ -3,7 +3,58 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+
+  //Primero divido la fecha en números dentro de un arreglo separados por la barra "/"
+  var arrFecha = fecha.split('/');
+  console.log(arrFecha);
   
+  //Están los números separados pero todavía son strings, así que tengo que hacer que el programa los lea como números. Creo un arreglo vacío
+  var arrFechaInt = [];
+
+  //Recorro el arreglo de strings y una por una la voy cambiando a entero.
+  for (let i = 0; i < arrFecha.length; i++)
+  {
+    arrFechaInt.push(parseInt(arrFecha[i], 10));
+  }
+  console.log(arrFechaInt);
+
+  //Listo, ya tenemos un arreglo de números enteros para verificar si el día, mes y fecha son correctos.
+  if (arrFechaInt[1] > 12 || arrFechaInt[1] < 1)
+  {
+    return false;
+  }
+  if (arrFechaInt[1] === 1 || arrFechaInt[1] === 3 || arrFechaInt[1] === 5 || arrFechaInt[1] === 7 || arrFechaInt[1] === 8 || arrFechaInt[1] === 10 || arrFechaInt[1] === 12)
+  {
+    if (arrFechaInt[0] > 31 || arrFechaInt[0] < 1)
+    {
+      return false;
+    }
+  }
+  else if (arrFechaInt[1] === 4 || arrFechaInt[1] === 6 || arrFechaInt[1] === 9 || arrFechaInt[1] === 11)
+  {
+    if (arrFechaInt[0] > 30 || arrFechaInt[0] < 1)
+    {
+      return false;
+    }
+  }
+  else if ((arrFechaInt[2] % 4 === 0 && arrFechaInt % 100 !== 0) || (arrFechaInt[2] % 100 === 0 && arrFechaInt % 400 === 0) )
+  {
+    if (arrFechaInt[0] > 29 || arrFechaInt[0] < 1)
+    {
+      return false;
+    }
+  }
+  else if (arrFechaInt[2] % 4 !== 0)
+  {
+    if (arrFechaInt[0] > 28 || arrFechaInt[0] < 1)
+    {
+      return false;
+    }
+  }
+
+  return true;
 }
+console.log(esFechaValida('35/01/2025'));
+
 
 module.exports = esFechaValida;
--- M05 Fundamentos Javascript/ejercicios/11.js
+++ M05 Fundamentos Javascript/ejercicios/11.js
@@ -5,7 +5,25 @@ function esMayorDeEdad(fechaNacimiento) {
   // Devuelve true si lo es, caso contrario, retorna false.
   // PISTA: Investiga el método getFullYear() de la clase Date para obtener el año actual.
   // Tu código:
-  
-}
+  var arrFecha = fechaNacimiento.split('/');
+  var arrInt = [];
 
+  for (let i = 0; i < arrFecha.length; i++)
+  {
+    arrInt.push(parseInt(arrFecha[i]));
+  }
+
+  // Existe un objeto de tipo Date que representa la fecha actual
+  var fechaActual = new Date();
+
+  // Y a su vez existe un método que te permite obtener el año actual, Ja!
+  var añoActual = fechaActual.getFullYear();
+
+  var mayor = function (){
+    return añoActual - 18 > arrInt[2] - 18;
+  }
+
+  return mayor();
+}
+console.log(esMayorDeEdad('12/01/2006s'));
 module.exports = esMayorDeEdad;
